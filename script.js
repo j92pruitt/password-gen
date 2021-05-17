dad59628bf6f7d1704e81f48fa33088d783e853c
j92pruitt <j92pruitt@gmail.com>
2021-05-16 17:20:58 -0700

    Added Psuedocode for script.

--- script.js
+++ script.js
@@ -10,7 +10,24 @@ function writePassword() {
 
 }
 
+// `generatePassword` function is called by `writePassword`. Returns `password` a random string based on user input criteria.
 function generatePassword(){
+// Create arrays containing all possible symbols, letters, and numbers for use in password.
+
+// PROMPT user for `passwordLength`.
+  // Validate that provided value is a number between 8 and 128. If not then repromt.
+
+// CONFIRM if user wants to have lowercase letters. If so add them to `char-array` and add 1 random lowercase letter to `password`
+
+// CONFIRM if user wants to have uppercase letters. If so add them to `char-array` and add 1 random uppercase letter to `password`
+
+// CONFIRM if user wants to have numbers. If so add them to `char-array` and add 1 random number to `password`
+
+// CONFIRM if user wants to have special characters. If so add them to `char-array` and add 1 random special character to `password`
+
+// Continue to add random characters from `char-array` to `password` UNTIL `password.length` is equal to the user provided `passwordLength`.
+
+// RETURN `password`
   return "Password"
 }
 
