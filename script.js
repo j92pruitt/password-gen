487b9e76bbf19d926727cdc4ad6ad762c7392185
j92pruitt <j92pruitt@gmail.com>
2021-05-16 18:00:08 -0700

    Added arrays of characters to be used in password generation.

--- script.js
+++ script.js
@@ -14,6 +14,30 @@ function writePassword() {
 function generatePassword(){
 // Create arrays containing all possible symbols, letters, and numbers for use in password.
 
+var lowercaseArray = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" 
+];
+
+console.log(lowercaseArray.length); //check to make sure all 26 letters are in array and array is correctly stored in memory.
+
+var uppercaseArray = [
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+];
+
+console.log(uppercaseArray.length); //check to make sure all 26 uppercase letters are in array and array is correctly stored in memory.
+
+var numberArray = [
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+]
+
+console.log(numberArray.length); //check to make sure all 10 digits are in array and array is correctly stored in memory.
+
+var specialCharArray = [
+  " ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"
+]
+
+console.log(specialCharArray.length); //check to make sure all 33 chars are in array and array is correctly stored in memory.
+
 // PROMPT user for `passwordLength`.
   // Validate that provided value is a number between 8 and 128. If not then repromt.
 
